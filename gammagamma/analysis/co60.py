5011cf3100a706fb5b2a397db1da7a9e543660f3
Nicholas Miklave <nicholas.miklave@gmail.com>
2020-02-16 18:20:11 -0500

    some comments for readibility

--- gammagamma/analysis/co60.py
+++ gammagamma/analysis/co60.py
@@ -9,18 +9,18 @@ mpl.rcParams['mathtext.fontset'] = 'stix'
 mpl.rcParams['font.family'] = 'STIXGeneral'
 
 
-def gaussian(x, a, m, s, o):
+def gaussian(x, a, m, s, o):    # Gaussian to fit to
     gauss = a*np.exp(-.5*((x-m)/s)**2)+o
     return gauss
 
 
-def gaussFit(x, y):
+def gaussFit(x, y):             # Fit the gaussian and return parameters
     inits = [max(y), 350, 10, np.mean(y)]
     params, covar = curve_fit(gaussian, x, y, p0=inits)
     return params
 
 
-def getData(theta):
+def getData(theta):             # Load data from file, using the angle tag in file name. Returns integrated counts
     fname = '../data/Co60_DetC_{0}_2.Spe'.format(str(theta).zfill(3))
     data = np.loadtxt(fname, skiprows=12, max_rows=2047)
     bins = np.arange(len(data))
@@ -29,9 +29,8 @@ def getData(theta):
     fit = gaussFit(*data[200:400].T)
     integral = quad(gaussian, fit[1]-3*fit[2], fit[1]+3*fit[2], args=(fit[0], fit[1], fit[2], fit[3]))[0]
 
-    #plt.plot(*data[200:400].T)
-    #plt.axvline(x=fit[1]-3*fit[2])
-    #plt.axvline(x=fit[1]+3*fit[2])
+    #plt.plot(*data[200:400].T)                 # If you want to view the gaussian fits
+    #plt.plot(*data.T[0], )
     #plt.show()
 
     sum_noBG = sum(data[100:400].T[1]) - fit[3]*201
@@ -42,7 +41,7 @@ def getData(theta):
     return integral
 
 
-def legendre(x, a0, a1, a2, a3, a4):
+def legendre(x, a0, a1, a2, a3, a4):        # Legendre polynomial fit
     p1 = x
     p2 = .5*(3*x**2-1)
     p3 = .5*(5*x**3-3*x)
@@ -51,11 +50,11 @@ def legendre(x, a0, a1, a2, a3, a4):
     return func
 
 
-def fit_legendre(x, y, yerr):
+def fit_legendre(x, y, yerr):               # Find the polynomial fit parameters and plot them
     inits = [1, 0, 0, 0, 0]
-    params, covar = curve_fit(legendre, x, y, sigma=yerr, p0=inits)
-    domain = np.linspace(x[0], x[-1], 200)
-    plt.plot(domain, legendre(domain, *params),
+    params, covar = curve_fit(legendre, np.cos(x), y, sigma=yerr, p0=inits)     # Legendre actually is cos(x)
+    domain = np.linspace(0, 140, 200)                                           # Range for plotting
+    plt.plot(domain, legendre(np.cos(domain), *params), 'r--',                  # Plot fit over range
              label='a0={0:2.4f}\na1={1:2.4f}\na2={0:2.4f}\na3={1:2.4f}\na4={2:2.4f}'.format(*params))
     return
 
@@ -78,7 +77,7 @@ def main():
     fit_legendre(angles, normed_rates, sig_normed_rates)
     plt.title("$^{60}$Co Coincidence Rate v. Detector Angle")
     plt.xlabel("Angle $(^\circ)$")
-    plt.ylabel("Rate (counts/sec)")
+    plt.ylabel("Rate (counts/sec) Normalized by Rate at $90^\circ$")
     plt.legend()
     #plt.savefig('../plots/co60.eps', format='eps')
     plt.show()
